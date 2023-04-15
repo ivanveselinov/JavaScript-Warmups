57c4f4fd9b1338688ea98221eb7e8dddda61f652
Ivan <ivanveselinov22@gmail.com>
2023-04-15 20:26:55 +1000

    More Functions

---
+++ 15.04.2023/functionsHomeWork/exercise.js
@@ -0,0 +1,18 @@
+const squareNumber = function (num1) {
+  square = num1 * num1
+  console.log(`Result is ${num1} * ${num1} = ${square}`)
+}
+
+squareNumber(3)
+squareNumber(5)
+
+const halfNumber = function (num1){
+  result = num1 / 2
+
+  console.log(`Half number of ${num1} is ${result}`)
+}
+
+halfNumber(2)
+halfNumber(4)
+halfNumber(6)
+halfNumber(8)
