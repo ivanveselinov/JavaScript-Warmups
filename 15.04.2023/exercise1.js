24f9dd153de1356f6abb56d1c9763f2f9f77ca8b
Ivan <ivanveselinov22@gmail.com>
2023-04-15 18:51:49 +1000

    More javaScript

---
+++ 15.04.2023/exercise1.js
@@ -0,0 +1,41 @@
+// The Fotune Teller
+
+let name = "Ivan"
+let location = "Melbourne"
+let partner = "Marina"
+let kids = 3
+
+console.log(`You will be ${name} in ${location}, and married to ${partner} with ${kids} kids`);
+
+
+// The life Time support calculator
+
+
+let currentYear = 2023;
+let birthYear = 1991;
+
+const calculatedAge = currentYear - birthYear
+const maybeAge = currentYear - birthYear - 1
+
+console.log(`You are either ${calculatedAge} or ${maybeAge} years old`);
+
+
+// The LifeTime Supply calculator
+
+let foodAmouthPerDay = 4;
+let death = 95;
+
+const timeUntilYouDie = death - (currentYear - birthYear)
+
+const foodSupply = (timeUntilYouDie * 365 ) * 3
+
+console.log(`You have left ${timeUntilYouDie} years to live and you will eat ${foodSupply} times until you die`);
+
+
+// The Temperature Converter
+
+let celsiusTemperature = 35;
+
+const fahreit = ((celsiusTemperature * 9 )/ 5 ) + 32
+
+console.log(`Tempareture is ${celsiusTemperature} Celsius or ${fahreit} fahreit`)
