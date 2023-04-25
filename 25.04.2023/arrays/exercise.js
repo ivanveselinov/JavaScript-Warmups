8220921af596f1b04bf6b1cbafed9d02ad2e08e7
Ivan <ivanveselinov22@gmail.com>
2023-04-25 15:07:56 +1000

    more function and array exercise

--- 25.04.2023/arrays/exercise.js
+++ 25.04.2023/arrays/exercise.js
@@ -16,4 +16,50 @@ for ( let i = 0; i < array.length; i++){
 
 }
 
-console.log(presidentFunction())
+console.log(presidentFunction());
+
+
+const maxOfTwoNumbers = function (num1, num2) {
+  if ( num1 > num2) {
+    console.log(`Number ${num1} is bugger then ${num2}`);
+  } else if ( num1 === num2) {
+    console.log(`Both numbers are ${num1}`);
+  } else {
+    console.log(`Number ${num2} is bigger then ${num1}`)
+  }
+}
+
+maxOfTwoNumbers(10, 20);
+maxOfTwoNumbers(20, 10);
+maxOfTwoNumbers(10, 10);
+
+const maxOfThreeNumbers = function (num1, num2, num3) {
+  if ( num1 > num2 && num1 > num3){
+    console.log(`Biggest number is ${num1} and other numbers are ${num2} and ${num3}`)
+  } else if ( num2 > num1 && num2 > num3) {
+    console.log(`Biggest number is ${num2} and other numbers are ${num1} and ${num3}`)
+  } else {
+    console.log(`Biggest number is ${num3} and other numbers are ${num1} and ${num2}`)
+  }
+}
+
+maxOfThreeNumbers(8,6,4)
+maxOfThreeNumbers(8,10,4)
+maxOfThreeNumbers(8,6,14)
+
+
+const character = function () {
+  array = ['test', 'i can do it again', 'does not matter', 'vovel'];
+
+  for ( let i = 0; i < array.length; i++ ) {
+    console.log(`${i} - ${array[i]}`);
+
+    if ( array[i] === "vovel"){
+      console.log(`We have found you ${array[i]}`)
+    } else {
+      console.log("False")
+    }
+  }
+}
+
+character()
